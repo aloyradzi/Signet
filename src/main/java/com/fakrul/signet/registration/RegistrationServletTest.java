fab68b0c13b3d9cb4e0450d8d4182ad1f0979105
Fakrul <aloyradzi@gmail.com>
2018-05-28 10:37:46 +0800

    Changes 1

--- src/main/java/com/fakrul/signet/registration/RegistrationServletTest.java
+++ src/main/java/com/fakrul/signet/registration/RegistrationServletTest.java
@@ -35,7 +35,7 @@ public class RegistrationServletTest extends HttpServlet {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet RegistrationServletTest</title>");            
+            out.println("<title>Servlet RegistrationServletTest Hehehe</title>");            
             out.println("</head>");
             out.println("<body>");
             out.println("<h1>Servlet RegistrationServletTest at " + request.getContextPath() + "</h1>");
